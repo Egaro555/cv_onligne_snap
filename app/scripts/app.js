9a9d7bdb32d3a0aacd6e557a904c9dcb97573d71
Egaro555 <hugues.vogel@hotmail.fr>
2016-11-20 18:45:54 +0100

    commit default empty Yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cvOnligneSnapApp
+ * @description
+ * # cvOnligneSnapApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cvOnligneSnapApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvOnligneSnapApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cvOnligneSnapApp
+ */
+angular.module('cvOnligneSnapApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
